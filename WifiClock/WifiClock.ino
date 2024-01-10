543455a2b1a93ff2cae91c29317826e84083d0cf
Jeremy Hourdin <jeremy.hourdin@gmail.com>
2024-01-10 21:36:18 +0000

    Project init for NodeMCU V3

---
+++ WifiClock/WifiClock.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  
+  Serial.begin(115200);
+  delay(10);
+  
+  WiFi.mode(WIFI_STA);
+  delay(500);
+
+  WiFi.beginSmartConfig();
+  
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if(WiFi.smartConfigDone()){
+      Serial.println("WiFi Smart Config Done.");
+    }
+  }
+  
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  
+}
